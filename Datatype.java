cc6abb8902d489f21ec45cc9ef07d49bc1cc7b4a
moulees2001 <113106315+moulees2001@users.noreply.github.com>
2022-09-08 21:17:38 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,25 @@
+
+class Datatype
+{
+public static void main (String [] args)
+{
+			byte age=21;
+		System.out.println(age);
+		short pinnum=257;
+		System.out.println(pinnum);
+		int regno=51081;
+		System.out.println(regno);
+		Long phno=984561234l;
+		System.out.println(phno);
+		float average=7.09f;
+		System.out.println(average);
+		double bill=7.009;
+		System.out.println(bill);
+		char gender='M';
+		System.out.println(gender);
+		String email="cat@132";
+		System.out.println(email);
+		boolean result=true;
+		System.out.println(result);
+}}
+
